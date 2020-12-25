5eb982becb0cf738163ed30745a213c00dbfabd2
MOHAMED RIAD <48014410+mriiad@users.noreply.github.com>
2020-12-26 00:49:19 +0100

    Add files via upload

---
+++ Day 6/Solution/Node.java
@@ -0,0 +1,12 @@
+package problem_66;
+
+/**
+ * @author m-riad
+ */
+public class Node {
+    public int val;
+    public Node next;
+    public Node(int val) {
+	this.val = val;
+    }
+}
---
+++ Day 6/Solution/SinglyLinkedListTest.java
@@ -0,0 +1,40 @@
+package problem_66;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * @author m-riad
+ */
+public class SinglyLinkedListTest {
+    
+    @Test
+    public void testCase1_isPalindrome() {
+	SinglyLinkedList sll = new SinglyLinkedList();
+	sll.add(new Node(1));
+	sll.add(new Node(3));
+	sll.add(new Node(4));
+	sll.add(new Node(3));
+	sll.add(new Node(1));
+	
+	// Test
+	assertEquals(true, sll.isPalindrome());
+    }
+    
+    @Test
+    public void testCase2_isPalindrome() {
+	SinglyLinkedList sll = new SinglyLinkedList();
+	sll.add(new Node(1));
+	sll.add(new Node(3));
+	sll.add(new Node(4));
+	sll.add(new Node(4));
+	sll.add(new Node(3));
+	sll.add(new Node(4));
+	sll.add(new Node(1));
+	
+	// Test
+	assertEquals(false, sll.isPalindrome());
+    }
+
+}
